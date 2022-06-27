312d78df30e484312b14d1e16c09b21d5827cc32
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-06-27 17:23:32 -0500

    Sesiones 25-26 - 27/06/2022

--- Programa.c
+++ Programa.c
@@ -1,45 +1,63 @@
-/* Este es mi primer programa en C */
-#include <stdio.h>			//  Para para usar printf(), scanf(), fflush(),gets()
-#include <stdlib.h>			//  Para usar system()
+#include <stdio.h>
+#include <stdlib.h>
 
 #define pausa system("pause")
-#define cls system("cls")
 
-// Funciones
 int main()
 {
-	const float PI = 3.14159;		// Declarqción de constantes
-	float pi = 3.014159;						// Declaración de una variable float llamada pi
-	float area = 0.00000025852;
-	int edad = 45;
-	char car = 'A';
-	char cad[19] = "Saludos Programas!!!\n";
-	int hora = 0, minutos;
-	//  scanf()
-	printf("Dame la edad: ");
-	scanf("%d", &edad);
-	printf("Dame la el area: ");
-	scanf("%f", &area);
-	fflush(stdin);
-	printf("Dame un caracter: ");
-	car = getchar();
-	printf("Dame la hora y munutos: ");
-	scanf("%d:%d", &hora, &minutos);
-	fflush(stdin);
-	printf("Dame una cadena: ");
-	gets(cad);
-	
-	//  printf()
-	printf("pi = %15.3f y area = %f\n", PI, area);		// invoco  printf(); es una función y es una sentencia
-	printf("Edad = %+-020d, car = %c = '%c', = %d \n", edad, car, car, car);
-	printf("Cadena = %s \n",cad);
-	printf("%s\n",cad);
-	printf("Impuesto del 25%% = %f \n", edad * 0.25);
-	printf("LA dirección de memoria de la variable edad es: %p\n", &edad);
-	printf("Hora: %d:%02d\n", hora, minutos);
-	
+	char nombre[20], paterno[20], materno[20];
+	char clave_elector[19], curp[19], direccion[30];
+	int registro, estado, municipio, seccion, localidad, emision, vigencia;
+
+	// Petición de Datos
+	printf("Nombre: \t\t");
+	gets(nombre);
+	printf("Apellido paterno: \t");
+	gets(paterno);
+	printf("Apellido Materno: \t");
+	gets(materno);
+	printf("Dirección: \t\t");
+	gets(direccion);
+	printf("Clave de elector: \t");
+	gets(clave_elector);
+	printf("CURP: \t\t\t");
+	gets(curp);
+	printf("Año de registro: \t");
+	scanf("%d", &registro);
+	printf("Estado: \t\t");
+	scanf("%d", &estado);
+	printf("Municipio: \t\t");
+	scanf("%d", &municipio);
+	printf("Sección: \t\t");
+	scanf("%d", &seccion);
+	printf("Localidad: \t\t");
+	scanf("%d", &localidad);
+	printf("Emisión: \t\t");
+	scanf("%d", &emision);
+	printf("Vigencia: \t\t");
+	scanf("%d", &vigencia);
 	pausa;
-	cls;
+
+	// Impresión de datos
+	printf("+---------------------------------------------------------+\n");
+	printf("|                        INSTITUTO NACIONAL ELECTORAL     |\n");
+	printf("|                        Credencial para Votar            |\n");
+	printf("| +--------+ Nombre:                                      |\n");
+	printf("| |///-\\\\\\ | %-20s                         |\n",nombre);
+    printf("| ||^   ^| | %-20s                         |\n",paterno);
+    printf("| ||O   O| | %-20s                         |\n",materno);
+	printf("| ||  ~ *| | Direcci½n:                                   |\n");
+    printf("| | \\ O /  | %-30s               ³\n",direccion);
+    printf("| |  | |   | Clave de Elector: %-20s       |\n",clave_elector);
+    printf("| | _| |_  | CURP: %-20s Registro: %4d    |\n",curp, registro);
+    printf("| |        | Estado: %2d  " , estado);
+    printf("Municipio: %2d  ", municipio);
+    printf("Secci½n: %2d       |\n", seccion);
+	printf("| +--------+ Localidad: %4d ",localidad);
+    printf("Emision: %4d  ", emision);
+    printf("Vigencia:%4d |\n", vigencia);
+	printf("|                                                         |\n");
+	printf("+---------------------------------------------------------+\n");
+
+	pausa;
 }
-
-
