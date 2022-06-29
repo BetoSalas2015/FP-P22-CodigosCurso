74e692ad5e2da02664da4614731db3649aaca86e
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-06-29 17:54:50 -0500

    Sesion 27-28 29/06/2022

--- Programa.c
+++ Programa.c
@@ -1,63 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* Este es mi primer programa en C */
+#include <stdio.h>			//  Para para usar printf(), scanf(), fflush(),gets()
+#include <stdlib.h>			//  Para usar system()
+#include <math.h>			//  Para usar pow(base, exp) y sqrt(numero)
 
 #define pausa system("pause")
+#define cls system("cls")
 
+// Funciones
 int main()
 {
-	char nombre[20], paterno[20], materno[20];
-	char clave_elector[19], curp[19], direccion[30];
-	int registro, estado, municipio, seccion, localidad, emision, vigencia;
+	int entero;
+	// 0 - false
+	// 1 y todo lo demás - true
+	//  Operador de igualdad (==)   Ej.   2 == 3 => 0
+	printf("2 == 3? => %d\n", (2 == 3) );
+	printf("3 == 3? => %d\n", 3 == 3 );
+	//  Operador de no igualdad (!=)   Ej.   2 != 3 => 1
+	printf("2 != 3? => %d\n", (2 != 3) );
+	printf("3 != 3? => %d\n", 3 != 3 );
+	//  Operador mayor que (>)
+	printf("2 > 3? => %d\n", (2 > 3) );
+	//  Operador mayor o igual que ( >= )
+	printf("3 >= 3? => %d\n", 3 <= 3 );
+	//  Operador menor que (<)
+	printf("2 < 3? => %d\n", (2 < 3) );
+	//  Operador menor o igual que ( <= )
+	printf("3 <= 3? => %d\n", 3 <= 3 );
 
-	// Petición de Datos
-	printf("Nombre: \t\t");
-	gets(nombre);
-	printf("Apellido paterno: \t");
-	gets(paterno);
-	printf("Apellido Materno: \t");
-	gets(materno);
-	printf("Dirección: \t\t");
-	gets(direccion);
-	printf("Clave de elector: \t");
-	gets(clave_elector);
-	printf("CURP: \t\t\t");
-	gets(curp);
-	printf("Año de registro: \t");
-	scanf("%d", &registro);
-	printf("Estado: \t\t");
-	scanf("%d", &estado);
-	printf("Municipio: \t\t");
-	scanf("%d", &municipio);
-	printf("Sección: \t\t");
-	scanf("%d", &seccion);
-	printf("Localidad: \t\t");
-	scanf("%d", &localidad);
-	printf("Emisión: \t\t");
-	scanf("%d", &emision);
-	printf("Vigencia: \t\t");
-	scanf("%d", &vigencia);
+	// Operadores Lógicos
+	//  Operador y lógico AND ( && )
+	printf("Tabla de Verdad del y (AND)\n");
+	printf("\ta\tb\t(a && b)\n");
+	printf("\t0\t0\t   %d\n", 0 && 0);
+	printf("\t0\t1\t   %d\n", 0 && 1);
+	printf("\t1\t0\t   %d\n", 1 && 0);
+	printf("\t1\t1\t   %d\n", 1 && 1);
+
+	//  Operador y lógico OR ( || )
+	printf("\nTabla de Verdad del O (OR)\n");
+	printf("\ta\tb\t(a || b)\n");
+	printf("\t0\t0\t   %d\n", 0 || 0);
+	printf("\t0\t1\t   %d\n", 0 || 1);
+	printf("\t1\t0\t   %d\n", 1 || 0);
+	printf("\t1\t1\t   %d\n", 1 || 1);
+
+	//  Operador negación lógica NOT ( ! )
+	printf("Tabla de Verdad de la negación (AND)\n");
+	printf("\ta\t   (!a)\n");
+	printf("\t0\t   %d\n", !0);
+	printf("\t1\t   %d\n", !1);
+
+	printf("\n\n-------Condicional Simple------------\n");
+	printf("DAme un numero: ");
+	scanf("%d", &entero);
+	if(entero > 2)
+	{
+		printf(" Es mayor que dos\n");
+	}
+
+	printf("\n---------Condicional Completa----------\n");
+	if(entero < 3)
+	{
+		printf(" Es menor que tres\n");
+	}
+	else
+	{
+		printf("No es menor que tres\n");
+	}
 	pausa;
-
-	// Impresión de datos
-	printf("+---------------------------------------------------------+\n");
-	printf("|                        INSTITUTO NACIONAL ELECTORAL     |\n");
-	printf("|                        Credencial para Votar            |\n");
-	printf("| +--------+ Nombre:                                      |\n");
-	printf("| |///-\\\\\\ | %-20s                         |\n",nombre);
-    printf("| ||^   ^| | %-20s                         |\n",paterno);
-    printf("| ||O   O| | %-20s                         |\n",materno);
-	printf("| ||  ~ *| | Direcci½n:                                   |\n");
-    printf("| | \\ O /  | %-30s               ³\n",direccion);
-    printf("| |  | |   | Clave de Elector: %-20s       |\n",clave_elector);
-    printf("| | _| |_  | CURP: %-20s Registro: %4d    |\n",curp, registro);
-    printf("| |        | Estado: %2d  " , estado);
-    printf("Municipio: %2d  ", municipio);
-    printf("Secci½n: %2d       |\n", seccion);
-	printf("| +--------+ Localidad: %4d ",localidad);
-    printf("Emision: %4d  ", emision);
-    printf("Vigencia:%4d |\n", vigencia);
-	printf("|                                                         |\n");
-	printf("+---------------------------------------------------------+\n");
-
-	pausa;
+	cls;
 }
+
+
