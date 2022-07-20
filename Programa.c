827637e6f4ac3d8f47186462fd851e2741b0d13e
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-07-20 16:47:12 -0500

    Tema Archivos

--- Programa.c
+++ Programa.c
@@ -1,56 +1,32 @@
-/* Otri programa en C  del Curso*/
-// LA criba de Eratóstenes
+/* Otro programa en C  del Curso*/
 #include <stdio.h>			//  Para para usar printf(), scanf(), fflush(),gets()
 #include <stdlib.h>			//  Para usar system(), rand() y srand
-#include <string.h>			//  Para usar las rutinas de cadenas
 
 #define pausa system("pause")
 #define cls system("cls")
-
-#define SIZE 80
+#define delay system("timeout 10")
+//  Prototipos
+int square(int num);
 
 // Funciones
-int main()
+//  Definición (Declaración) de Funcion  
+
+
+void main()
 {
-	char cadena[SIZE];
-	int i, j;
-
-	printf("Dame una cadena a analizar: ");
-	gets(cadena);
-
-	i = 0;
-	while(cadena[i] != '\0')
-	{
-		if(cadena[i] >= 'a' && cadena[i] <= 'z')		// si es una letra minuscula
-			cadena[i] -= 32;
-		i++;
-	}
-
-	j = i - 1;				// nos posicionamos al final de la cadena
-	i = 0;					//  y al principio de la misma
-
-	while(i <= j)
-	{
-		if(cadena[i] == ' ')
-			i++;
-		if(cadena[j] == ' ')
-			j--;
-		if(cadena[i] != cadena[j])
-		{
-			printf("La cadena no es palindroma\n");
-			pausa;
-			exit(0);
-		}
-		i++;
-		j--;
-	}
-	puts("LA cadena fue palindroma");
-	
-	
+	int x, res;
+	delay;
+	system("chcp 850");
+	printf("Dame un número:");
+	scanf("%d", &x);
+	res = square(x);
+	// Invocación de una función
+	printf("El resultado de elevar %d al cuadrado es: %d\n", x, res );
 	pausa;
 	cls;
 }
 
-
-
-
+int square(int num) 
+{
+	return (num * num); 
+}
